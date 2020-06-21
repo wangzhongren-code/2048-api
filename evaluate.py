1b360124395bcd749f943c761c27953f91bf885b
wangzhongren-code <52587554+wangzhongren-code@users.noreply.github.com>
2020-06-21 22:08:30 +0800

    this is for getting the EE369_evaluation.log

--- evaluate.py
+++ evaluate.py
@@ -1,6 +1,6 @@
 from game2048.game import Game
 from game2048.displays import Display
-
+from game2048.agents import Agent, RandomAgent, ExpectiMaxAgent,YourOwnAgent,SimpleNet
 
 def single_run(size, score_to_win, AgentClass, **kwargs):
     game = Game(size, score_to_win)
@@ -12,17 +12,18 @@ def single_run(size, score_to_win, AgentClass, **kwargs):
 if __name__ == '__main__':
     GAME_SIZE = 4
     SCORE_TO_WIN = 2048
-    N_TESTS = 50
+    N_TESTS = 10
 
     '''====================
     Use your own agent here.'''
-    from game2048.agents import ExpectiMaxAgent as TestAgent
+    from game2048.agents import YourOwnAgent as TestAgent
     '''===================='''
 
     scores = []
-    for _ in range(N_TESTS):
+    for i in range(N_TESTS):
         score = single_run(GAME_SIZE, SCORE_TO_WIN,
                            AgentClass=TestAgent)
         scores.append(score)
+        print(score) 
 
     print("Average scores: @%s times" % N_TESTS, sum(scores) / len(scores))
